2942bc9dc189b7823c4b3cc304f9a1226e2f51e0
lipesco <filipe.branco@gmail.com>
2018-03-09 20:07:02 +0000

    adição do código de manipulação dos algarismos

--- CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -9,8 +9,40 @@ namespace CalculadoraCompleta.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        [HttpGet]  // Esta anotação é facultativa, pois, por defeito já tem GET sem qualquer anotação
         public ActionResult Index()
         {
+            //iniciarlizar o valor do visor
+            ViewBag.Visor = 0;
+            return View();
+        }
+        
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(string bt, string visor)
+        {
+            // identificar o valor da variável 'bt'
+            switch (bt)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    if (visor.Equals("0"))
+                        visor = bt;
+                    else
+                        visor += bt;
+                    break;
+                default:break;
+            }
+            //enviar resposta para o cliente
+            ViewBag.Visor = visor;
+
             return View();
         }
     }
